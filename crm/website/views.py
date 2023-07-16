b2dac98c648ec75e5aa041140f8d59c11c5300c6
Madhav aryal <aryalmadhave123@gmail.com>
2023-07-16 22:49:09 +0545

    registration

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+def home(request):
+    #check to see if logging in 
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        #authenticate
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"you have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request,"there was a error please log in again!")
+            return redirect('home')
+    else:
 
-# Create your views here.
+        return render(request,'home.html',{})
+
+def logout_user(request):
+    
+
+    logout(request)
+    messages.success(request,"you have been logged out!")
+    return redirect('home')
+
+def register_user(request):
+    return render(request,'register.html',{})
